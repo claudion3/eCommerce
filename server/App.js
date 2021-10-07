7bf015f6c58b0c4d9b68c28ae4a90f5e41f0b518
claudion3 <claudion3@gmail.com>
2021-10-07 11:23:29 +0200

    working in progress...

--- server/App.js
+++ server/App.js
@@ -2,14 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
-
+const cors = require('cors');
 connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/products', productRoutes);
-
 const PORT = process.env.API_PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
