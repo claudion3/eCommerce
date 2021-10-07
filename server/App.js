51bec097e60a83747b333384fecd7d79e03a8cd3
claudion3 <claudion3@gmail.com>
2021-10-07 11:58:29 +0200

    update package.json

--- server/App.js
+++ server/App.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
+
 connectDB();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(cors());
-
+app.use(bodyParser.json());
 app.use('/api/products', productRoutes);
-const PORT = process.env.API_PORT || 5000;
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
